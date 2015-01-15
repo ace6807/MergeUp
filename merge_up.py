28f43d956ad5e04a86573d8248b11baefa4d5f97
Andrew Yatsko <ayatsko2@live.esu.edu>
2015-01-15 10:19:55 -0500

    MergeUp working. TODO: One-click installer for windows Context entry for Linux

---
+++ merge_up.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+
+def merge_up(master_folder):
+    sub_dirs = os.listdir(master_folder)
+    if len(sub_dirs) == 0:
+        print("Success")
+    for item in sub_dirs:
+        path = os.path.abspath(master_folder + slash + item)
+        if os.path.isdir(path):
+            merge(path, master_folder)
+
+
+def merge(path, master_folder):
+    if os.path.isdir(path):
+        sub_items = os.listdir(path)
+        for item in sub_items:
+            merge(path + slash + item, master_folder)
+        try:
+            os.rmdir(path)
+        except:
+            print("Warning: Directory is not empty, cannot be deleted")
+    else:
+        move_file_to_master(path, master_folder)
+
+
+def move_file_to_master(path, master_folder):
+    split_path = os.path.split(path)
+    src_path = split_path[0]
+    filename = split_path[1]
+    split_filename = filename.split(".")
+    basename = split_filename[0]
+    file_extension = split_filename[1]
+
+    while os.path.isfile(master_folder + slash + basename + "." + file_extension):
+        basename += "9"
+
+    new_file = master_folder + slash + basename + "." + file_extension
+    os.rename(path, new_file)
+
+slash = '\\' if sys.platform.startswith('win') else '/'
+merge_up(sys.argv[1])
